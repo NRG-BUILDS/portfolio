e62cec37c789a517b1d795212c32ceb32491a458
NRG-BUILDS <111093908+NRG-BUILDS@users.noreply.github.com>
2022-09-20 19:33:54 +0100

    Add files via upload

--- quiz/main.js
+++ quiz/main.js
@@ -1,10 +1,12 @@
 const welcome = document.querySelector('.welcome')
+const user = document.querySelector('#user')
+const startBtn = document.querySelector('.start')
 const displayContainer = document.querySelector('.app_container')
 const display = document.querySelector('.app')
 const nextBtn = document.querySelector('.next')
 const resultCard = document.querySelector('.result_card')
 const questionArray = [] //houses question objects
-const answerArray = []
+const answerArray = [] //houses answers which will be listed out at quiz end
 //create question object constructor
 function Question(qNumber, question, optionBox, answer) { 
     questionArray.push(this); //automatically adds questions to questionArray
@@ -28,74 +30,78 @@ function Question(qNumber, question, optionBox, answer) {
 
 
 
-
 //create question objects
-let qu1 = new Question (
+const qu1 = new Question (
     "Question 1", 
-    "Verify your name (plus get free point)",
-    ["Seun", "Sharon", "Mubarak", "Kunle", "Favour", "Hi, I'm a group ghost👻"], 
-    "All"
+    "Which of the following is a markup language?",
+    ["HTML", "CSS", "JAVASCRIPT", "French"], 
+    "HTML"
 )
-console.log(qu1.options)
+//console.log(qu1.options)
 
-let qu2 = new Question (
+const qu2 = new Question (
     "Question 2", 
-    "Who Is Taller?",
-    ["Favor", "Dronix", "Emmanuel"],
-    "Dronix"
+    "Who Is Taller At Noon?",
+    ["A Man", "His Thirst", "His Shadow"],
+    "His Thirst"
 )
-let qu3 = new Question (
+const qu3 = new Question (
     "Question 3", 
     "What Was Queen Elizabeth's Surname?",
     ["The Second😂", "II", "Windsor", "Buckingham"],
     "Windsor"
 )
-let qu4 = new Question (
+const qu4 = new Question (
     "Question 4", 
-    "Which Came First?",
+    "According to evolution, Which Came First?",
     ["The Chicken", "The Egg", "Charles Darwin"],
-    "The Chicken"
+    "The Egg"
 )
-let qu5 = new Question(
+const qu5 = new Question(
     "Question 5",
-    "Who Was Recently Crowned \"Most Handsome\" In The Group??",
-    ["Emmanuel (this is a trick question. I'm the answer. PICK ME IF YOU WANT TO LIVE!!!)", "Kunle", "Seun", "Favour", "Mubarak"],
-    "Emmanuel (this is a trick question. I'm the answer. PICK ME IF YOU WANT TO LIVE!!!)"
+    "Who Is The Most Handsome Dev You Know?",
+    ["NRG (this is a trick question. I'm the answer. PICK ME IF YOU WANT TO LIVE!!!)", "Yourself", "Your Mentor"],
+    "NRG (this is a trick question. I'm the answer. PICK ME IF YOU WANT TO LIVE!!!)"
 )
-let qu6 = new Question(
+const qu6 = new Question(
     "Question 6",
     "I'm Running Out Of Qs At This Point. What Do You Think Next Q Is About?",
     ["Math", "History", "Tech"],
     "History"
 )
-let qu7 = new Question(
+const qu7 = new Question(
     "Question 7",
-    "What Was Used As Condoms Before Condoms?",
-    ["Special latex flowers", "Animal intestines", "Clothes", "Prayers"],
-    "Animal intestines"
-)
-let qu8 = new Question(
-    "Question 8",
-    "Want Me To Teach You JavaScript, you'd pay?",
-    ["Yes", "No", "Hehe, you get a point either way...", "...Except you're Kunle😂"],
+    "What Did You Have For Breakfast (this is a history question)",
+    ["Bread", "Noodles", "Fasting", "Prayers", "Rice", "Motivational Speech", "Hehe..don't worry, all answers are correct"],
     "All"
 )
-let qu9 = new Question(
-    "Question 9",
-    "Who's Vns 'sound like a penis'?",
-    ["Dah Boy", "Aidrop Boy", "Mr. sexismin iless", "Iniiii!!!!", "Imaginary glowups", "Sha-run"],
-    "Imaginary glowups"
+const qu8 = new Question(
+    "Question 8 (Since Morning)",
+    "Solve x<sup>2</sup> - 12 = -12",
+    ["12", "24", "0", "End ASUU Strike🥲"],
+    "0"
 )
-let qu10 = new Question(
+const qu9 = new Question(
+    "Question 9",
+    "Whic Superhero Is Likely To Be A Good WEB Developer?",
+    ["Batman", "Iron Man", "Thor", "Spider Man", "Cyborg", "Wonder Woman"],
+    "Spider Man"
+)
+const qu10 = new Question(
     "Question 10",
-    "This was fun making. Even though, it will take y'all 3 mins to complete, was it fun?",
-    ["Yes!", "Meh", "I feel personally attacked because I don't understand jokes and now I'm angry"],
-    "Yes!"
+    "This was fun making. Even though, it will take y'all 2 mins to complete, was it fun?",
+    ["Yes!", "No", "Either ways, you get another free point😘"],
+    "All"
 );
 
 //next button calls save() and changeQuestion() function
 nextBtn.addEventListener("click", function() {save()})
-//nextBtn.addEventListener("click", function() {changeQuestion()})
+
+//
+startBtn.addEventListener("click", function() {changeQuestion()})
+startBtn.addEventListener("click", function() {
+    username = user.value
+})
 
 //=========Counters
 //score adds +1 at every correct answer
@@ -104,6 +110,7 @@ const score = (function() {
     return function() { counter += 1; return counter}
 })()
 //start adds +1 in order to change question number
+// "qu" is a variable that will be used as a "global index". qu increases per changeWuestion()
 const start = (function() { 
     qu = -1;
     return function() { qu += 1; return qu}
@@ -114,41 +121,48 @@ function changeQuestion() {
     
         start()
         if (qu > questionArray.length - 1) {
-            displayContainer.style.display = "none"
-            resultCard.style.display = "block"
-        }//removes quiz card when last question has been answered
+            getResults()
+        }//Goes to results page when last question has been answered
         
         current = `<div class="active"><h2>${questionArray[qu].num}</h2> \
         <p>${questionArray[qu].quest}</p> \
         ${questionArray[qu].optionList()}</div>`;
         display.innerHTML = current
-    };    
-
-//changeQuestion()//declared to initialize first question.
+    }; //this function is called by the Start Quiz button to initialize first question.
 
 function save() { 
     
     let chosenOption = document.querySelector('input[name="option"]:checked').value;//get value of chosen option. If no option is chosen, this function will automatically be disabled
     
     let ansLen = answerArray.length
-    for (let i = 0; i < ansLen; i++) {
-        if (chosenOption == answerArray[i]) {
+   
+    if (chosenOption == questionArray[qu].ans) {
         score();
-        }//scores if chosen answer is part of answers in array
-    }
+    }//scores if chosen answer matches object answer
+    
     if (questionArray[qu].ans == "All") {
         score();
     }//scores if a question has All options as answers
-    document.getElementById('result').innerHTML = counter
     changeQuestion();
-    //console.log(counter)
+    console.log(counter)
     //console.log(chosenOption)
 }
+function getResults() { 
+    displayContainer.style.display = "none"
+    resultCard.style.display = "block"
+    
+    resultCard.innerHTML = `<p>Nice Work ${username}</p>
+        <h1>${counter}/${questionArray.length}</h1>
+        <p>As for answers...</p>
+        ${showAnswers()}
+        <a href="/"><button>Retry</button></a>`
+}
 
-
-
-
-
-
-
-console.log(questionArray.length)
+function showAnswers() {
+    let text = "<ol>"
+    for (i = 0; i < answerArray.length; i++ ) {
+        text += "<li>" + answerArray[i] + "</li>"
+    }
+    text += "</ol>"
+    return text
+}
