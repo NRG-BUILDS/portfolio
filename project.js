655944b695d517ea1415efbfcd5bad6c81f4c6b5
Emmanuel Omolaju <111093908+NRG-BUILDS@users.noreply.github.com>
2022-12-12 21:56:26 +0100

    Add files via upload

--- project.js
+++ project.js
@@ -1,8 +1,6 @@
 const projectArray = []//houses all projects
 
-
 function Projects( Name, PicSrc, Src) {
-    
     projectArray.push(this)// auto add new project objects to Array
     this.name = Name;
     this.pic = PicSrc;
@@ -25,29 +23,27 @@ const project4 = new Projects(
     "Hotel Landing Page (Mobile and PC)",
     "hotel.jpg",
     "./hotel/")
-const project5 = new Projects( 
-    "Online Music Streaming App",
-    "music.jpg",
-    "./music-player/")    
-const project6 = new Projects(
+const project5 = new Projects(
     "A Bunch Of JavaScript Calculators",
     "calculator.jpg",
     "./calculator/")    
-    
-    
-//const projectArray = [project1, project2, project3, project4, project5];
+const project6 = new Projects( 
+    "Online Music Streaming App",
+    "music.jpg",
+    "./music-player/")    
 
-let text = ""
-for (i in projectArray) { 
-    text += `<div class="projectItem">
+function showProjects() { 
+    let text = ""
+    for (i = projectArray.length - 1; i > -1; i--) { 
+        text += `<div class="projectItem">
             <a href= ${projectArray[i].src}>
             <img src= ${projectArray[i].pic}>
             <h5 class="swoop">${projectArray[i].name}</h5 class="swoop"></a>
         </div>`
-}
-document.getElementById('projectContainer').innerHTML = text
-
-
+    }
+    document.getElementById('projectContainer').innerHTML = text
+} 
+showProjects()
 
 
 
